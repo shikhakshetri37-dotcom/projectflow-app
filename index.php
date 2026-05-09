7bda42111240a2c49f0059ed5419530d2322e1f6
shikhakshetri37-dotcom <shikhakshetri37@gmail.com>
2026-05-09 13:45:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 require_once __DIR__ . '/config/session.php';
 
 if (isLoggedIn()) {
-    header('Location: /php-pmapp/dashboard/index.php');
+    header('Location: /dashboard/index.php');
 } else {
-    header('Location: /php-pmapp/auth/login.php');
+    header('Location: /auth/login.php');
 }
 exit;
+?>
